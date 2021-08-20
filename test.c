8bbeccbf25bdcc23b26d85c400f11efd36f9e14c
daimalvtu <88648349+daimalvtu@users.noreply.github.com>
2021-08-20 21:58:43 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	/*printf("%d\n",sizeof(int*));
+	printf("%d\n",sizeof(short*));
+	printf("%d\n", sizeof(char*));
+	printf("%d\n", sizeof(double*));*/
+
+	///*int a = 0x112233;
+	//int* pc = &a;
+	//char* pa = &a;
+	//printf("%p\n", pc+1);
+	//printf("%p\n", pa+1);*/
+	/*int arr[10] = {0};
+	char* p1 = arr;*/
+	///*int* p2 =&arr[0];
+	//int* p3 = &arr;
+	//printf("%p\n",p1);
+	//printf("%p\n", p2);
+	//printf("%p\n", p3);*/
+	int i = 0;
+	/*for (i = 0; i < 10; i++)
+	{
+		
+		*(p1 + i) = 1;
+		printf("arr[%d]=%d\n",i,arr[i]);
+	}*/
+	//数组地址和数组首元素地址的不同
+	/*int arr[10] = { 0 };
+	int* p = &arr;
+	printf("%p\n",&arr);
+	printf("%p\n", p);
+	
+	printf("%p\n",p+1);*/
+
+	return 0;
+}
